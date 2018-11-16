e67d540c2ea66fe631ebd491269fb9da23126192
Christian <kisquian@gmail.com>
2018-11-15 22:21:36 -0300

    User add, get, edit, and delete

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,44 @@
 // Imported modules
-
+const User = require('../models/user');
 
 // Exported modules
+module.exports = {
+    index:  async (req, res, next) => {
+      const user = await User.find({});
+      res.status(200).json(user);
+    },
+  
+      newUser: async (req, res, next) => {
+          const newUser = new User({userName: req.body.userName, userEmail: req.body.userEmail, userAge: req.body.userAge, userSex: req.body.userSex, userStatus: req.body.userStatus, userCountry: req.body.userCountry, userCity: req.body.userCity, userLikes: req.body.userLikes, userDislikes: req.body.userDislikes, userSexInterest: req.body.userSexInterest });
+          console.log(req.body);
+          const user = await newUser.save();
+          res.status(201).json(user);
+      },
+  
+      getUser: async(req, res, next) => {
+        const { userId } = req.params;
+        const user = await User.findById(userId);
+        res.status(200).json(user);
+      },
+
+      getUser: async(req, res, next) => {
+        const { userId } = req.params;
+        const user = await User.findById(userId);
+        res.status(200).json(user);
+      },
+  
+      updateUser: async(req, res, next) => {
+        const { userId }  =  req.params;
+        const newUser = req.body;
+        const oldUser = await User.findByIdAndUpdate(userId, newUser);
+        res.status(200).json({successs: true});
+      },
+  
+      deleteUser: async(req, res, next) => {
+        const { userId } = req.params;
+        await User.findOneAndDelete(userId);
+        res.status(200).json({success: true});
+      }
+  
+  
+  };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,16 @@
 // Imported modules
+
 const express = require('express');
 const router = express.Router();
 
+const { index, newUser, getUser, updateUser, deleteUser } = require('../controllers/users');
+
+router.get('/', index);
+router.post('/', newUser);
+router.get('/:userId', getUser);
+router.put('/:userId', updateUser);
+router.delete('/:userId', deleteUser);
 
 // Exported modules
 module.exports = router;
+
